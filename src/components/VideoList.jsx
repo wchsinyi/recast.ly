82603b4101e9267bdee08a43c5fd4a2d67bb924d
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-04-22 17:38:35 -0700

    Render videoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,19 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
 var VideoList = () => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {exampleVideoData.map(v => (
+      <VideoListEntry v={v} />
+    ))}
   </div>
+  // <div className="video-list">
+  // <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
